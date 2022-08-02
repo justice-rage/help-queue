1ad0f6ea311fdacc747f5e1839a82586e95bc82d
justicepelteir <justice.douglas@hey.com>
2022-08-01 19:37:33 -0700

    create header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 function App(){
   const name = "Thato";
   const name2 = "Haley";
   return (
     <React.Fragment>
+      <Header />
       <h1>Help Queue</h1>
       <h3>3a</h3>
       <h3>{name} and {name2}</h3>
