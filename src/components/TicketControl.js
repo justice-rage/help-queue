f125352ca83974101721711e3d7ca12bd7d3b086
justicepelteir <justice.douglas@hey.com>
2022-08-02 19:02:12 -0700

    add toggle logic

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -9,18 +9,29 @@ class TicketControl extends React.Component {
         this.state = {
             formVisibleOnPage: false
         };
+        this.handleClick = this.handleClick.bind(this);
     }
+
+    handleClick = () => {
+        this.setState(prevState => ({
+          formVisibleOnPage: !prevState.formVisibleOnPage
+        }));
+      }
 
     render(){
         let currentlyVisibleState = null;
+        let buttonText = null;
         if (this.state.formVisibleOnPage) {
-            currentlyVisibleState = <NewTicketForm />
+            currentlyVisibleState = <NewTicketForm />;
+            buttonText = "Return to Ticket List"; 
         } else {
-            currentlyVisibleState = <TicketList />
+            currentlyVisibleState = <TicketList />;
+            buttonText = "Add Ticket"; 
         }
         return (
             <React.Fragment>
                 {currentlyVisibleState}
+                <button onClick={this.handleClick}>{buttonText}</button>
             </React.Fragment>
         );
     }
